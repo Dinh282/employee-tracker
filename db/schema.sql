0a8339fe79476cff3b44c3beae4153e16c71df5b
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-06-02 22:28:34 -0400

    add code to server.js file. Modify schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,28 @@
 DROP DATABASE IF EXISTS company_employees_db;
-CREATE DATABASE course_db;
+CREATE DATABASE company_employees_db;
 
 USE company_employees_db;
 
 -- Create department table:
 CREATE TABLE department (
-  id INT,
-  company_name VARCHAR(30),
-  PRIMARY KEY (id),
+  id INT PRIMARY KEY,
+  company_name VARCHAR(30)
 );
 
 -- Create role table:
 CREATE TABLE role (
-  id INT,
+  id INT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
+  department_id INT,
   FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NUll,
-  PRIMARY KEY (id),
-)
+    ON DELETE SET NUll
+);
 
 -- Create employee table:
 CREATE TABLE employee (
-    id INT,
+    id INT PRIMARY Key,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
@@ -31,6 +30,7 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id)
         REFERENCES role(id)
         ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
-    
-)
+    FOREIGN KEY (manager_id) 
+        REFERENCES employee(id) 
+        ON DELETE SET NULL
+);
---
+++ server.js
@@ -0,0 +1,19 @@
+import mysql2 from 'mysql2';
+
+//Creates connection from Nodejs to database engine on local machine with .createConnection.
+//We are required to pass an object with the following properties. 
+const db = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'company_employees_db',
+});
+
+db.query('select * from employee', function(err, data){
+    if (err) {
+        console.log("Error!", err);
+    } else {
+        console.log('here is the data:', data);
+    }
+
+})
